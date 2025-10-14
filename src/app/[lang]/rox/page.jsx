9bc21476b9651885256894577573a28d935ceb98
automillennium <gokul.k@automillennium.com>
2025-10-14 14:46:07 +0400

    fix : -- our teams route fix with lang -- rox to projects route dynamic with lang

--- src/app/[lang]/rox/page.jsx
+++ src/app/[lang]/rox/page.jsx
@@ -6,6 +6,7 @@ import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
 import EmblaCarousel from "@/app/components/EmblaCarousal";
 import TestimonialsSection from "@/app/components/Testmonials";
+import Link from "next/link";
 
 // --- GSAP Setup ---
 gsap.registerPlugin(ScrollTrigger);
@@ -64,7 +65,9 @@ const FALLBACK_POSTER_URL = "/img/vtx-fallback-poster.jpg";
 const LOGO_URL = "/img/rox-white.png";
 
 // --- Main Component ---
-const Page = () => {
+const Page = ({ params }) => {
+  const { lang } = params;
+
   const [isVideoReady, setIsVideoReady] = useState(false);
   const [videoError, setVideoError] = useState(false);
   const videoRef = useRef(null);
@@ -308,8 +311,9 @@ const Page = () => {
               >
                 <div className="flex flex-col">
                   {categoryData.links.map((link, linkIndex) => (
-                    <a
-                      href={link.link}
+   // URL: http://localhost:3000/en/projects/rolls-royce
+<Link href={`/${lang}/${link.link}`}
+                      
                       key={linkIndex}
                       className={`
                         group relative flex items-center justify-between w-full text-[17px] font-light py-5 px-4
@@ -323,7 +327,7 @@ const Page = () => {
                         ›
                       </span>
                       <div className="absolute inset-0 rounded-xl bg-gradient-to-r from-transparent via-white/[0.02] to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
-                    </a>
+                    </Link>
                   ))}
                 </div>
               </div>
